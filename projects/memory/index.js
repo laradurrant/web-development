6e57253d60fd1ad18245497015526a82dee0b36c
Lara <lara.durrant@gmail.com>
2020-02-12 09:17:48 -0800

    Minor performance fixes for memory game

--- projects/memory/index.js
+++ projects/memory/index.js
@@ -1,3 +1,5 @@
+var waitTime = 3000;
+
 var list = [];
 var matches = 0;
 var squares = document.getElementsByClassName("square");
@@ -6,8 +8,13 @@ var pairs = count / 2;
 var characterList = ["☆", "♡", "♢", "☐", "♫", "♪", "♤", "♧"];
 var characters = [];
 
+var resetButton = document.getElementById("reset");
+var winText = document.getElementById("Win Text");
+var statusText = document.getElementById("status");
 
-document.getElementById("reset").addEventListener("click", reset); 
+resetButton.addEventListener("click", reset); 
+
+
 
 function createList(){
   for(var i = 0; i < count / 2; i++){
@@ -42,60 +49,77 @@ function makeBoard(){
   
 }
 
-createList();
-shuffleSquares();
-makeBoard();
 
 function addListener(){
 
-      var current = {
-        value: this.innerHTML, 
-        id: this.id
-      };
-      
-      this.classList.add("active");
-      this.removeEventListener("click", addListener);
-      addPair(current);
-    
+  // sets up our basic tile object
+  var current = {
+    value: this.innerHTML, 
+    id: this.id
+  };
+  
+  this.classList.add("active");
+  this.removeEventListener("click", addListener);
+  addPair(current);
+
+}
+
+function disableResetButton(){
+  resetButton.removeEventListener("click", reset); 
+  resetButton.classList.add("processing");
+  resetButton.innerHTML = "Start memorizing... ;)";
+}
+
+function enableResetButton(){
+  resetButton.addEventListener("click", reset); 
+  resetButton.classList.remove("processing");
+  resetButton.innerHTML = "Reset";
 }
 
 function reset(){
-  document.getElementById("reset").removeEventListener("click", reset); 
-  document.getElementById("reset").classList.add("processing");
-  document.getElementById("reset").innerHTML = "Start memorizing... ;)";
+
+  disableResetButton();  
   shuffleSquares();
  
   list.length = 0;
   matches = 0;
-  document.getElementById("Win Text").innerHTML = "";
-//   document.getElementById("status").innerHTML = "OK, here's your next set!";
-  
+  winText.innerHTML = "";
+    
     for(var i = 0; i < squares.length; i++){
  
       squares[i].classList.add("highlighted");
     }
-  setTimeout(displayBoard, 3000);
+  
+  
+    // will wait for a certain amount of time
+    // gives the player a chance to memorize the board
+    // can be modified for an easier game
+  setTimeout(displayBoard, waitTime);
   
    
 }
 
+
 function displayBoard(){
+  
+  // clears out & removes any existing styling
    for(var i = 0; i < squares.length; i++){
       squares[i].classList.remove("selected");
       squares[i].classList.remove("active");
       squares[i].classList.remove("highlighted");
     }
-   document.getElementById("Win Text").innerHTML = "";
-  document.getElementById("status").innerHTML = "";
+
+   winText.innerHTML = "";
+   statusText.innerHTML = "";
   
    makeBoard();
-  document.getElementById("reset").addEventListener("click", reset); 
-  document.getElementById("reset").classList.remove("processing");
-  document.getElementById("reset").innerHTML = "Reset";
+   enableResetButton();
+
  
 }
 
 function addPair(current){
+  // adds an element to the list & processes it to see if we have a pair
 
   if(list.length < 2){ 
      list.push(current); 
@@ -112,27 +136,32 @@ function addPair(current){
 
 function checkPairs(){
     if(list.length ==  2){
+
+       // If we have a matching pair
        if(list[0].value === list[1].value){ 
        
          matches++;
          document.getElementById(list[0].id).removeEventListener("click", addListener);
          document.getElementById(list[1].id).removeEventListener("click", addListener);
          
-        document.getElementById(list[0].id).classList.remove("active");
-        document.getElementById(list[1].id).classList.remove("active");
-        document.getElementById(list[0].id).classList.add("selected");
-        document.getElementById(list[1].id).classList.add("selected");
+         document.getElementById(list[0].id).classList.remove("active");
+         document.getElementById(list[1].id).classList.remove("active");
+         document.getElementById(list[0].id).classList.add("selected");
+         document.getElementById(list[1].id).classList.add("selected");
          
+         // If we have completed all of the pairs
+         // displays win screen
          if(matches === pairs) {
-            document.getElementById("Win Text").innerHTML = "You win!";
+           winText.innerHTML = "You win!";
            for(var i = 0; i < squares.length; i++){
  
-      squares[i].classList.add("highlighted");
-    }
+              squares[i].classList.add("highlighted");
+            }
          }
      }
     else {
-       
+        // Not a match, continue with the game & reset the styling
+
         document.getElementById(list[0].id).addEventListener("click", addListener);
         document.getElementById(list[1].id).addEventListener("click", addListener);
       
@@ -145,3 +174,13 @@ function checkPairs(){
 
 }
 
+
+function init(){
+  reset();
+  createList();
+  shuffleSquares();
+  makeBoard();
+  
+}
+
+init();
